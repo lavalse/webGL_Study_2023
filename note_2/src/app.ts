f67e4ed92294999e96a4219820575a03b9ffdcb7
XU Cong <lavalselike@gmail.com>
2024-04-25 00:28:20 +0900

    try uniform and varying way

--- note_2/src/app.ts
+++ note_2/src/app.ts
@@ -7,17 +7,23 @@ let gl: WebGLRenderingContext = <WebGLRenderingContext> canvas.getContext("webgl
 //vertexShader, fragmentShader
 let vertexSource = `
 attribute vec2 a_position;
-attribute float a_size;
+uniform float u_size;
+varying vec2 v_position;
 
 void main(){
+  v_position = a_position;
   gl_Position = vec4(a_position,0.0,1.0);
-  gl_PointSize = a_size;
+  gl_PointSize = u_size;
 }
 `;
 
 let fragmentSource = `
+precision mediump float;
+uniform vec3 u_color;
+varying vec2 v_position;
+
 void main(){
-  gl_FragColor = vec4(1.0,0.0,0.0,1.0);
+  gl_FragColor = vec4(u_color.x,v_position,1.0);
 }
 `;
 
@@ -39,8 +45,11 @@ for(let i=0; i<n; i++){
   const a_position = gl.getAttribLocation(webGLProgram, "a_position");
   gl.vertexAttrib2f(a_position, x, y);
 
-  const a_size = gl.getAttribLocation(webGLProgram, "a_size");
-  gl.vertexAttrib1f(a_size, size);
+  const u_size = gl.getUniformLocation(webGLProgram, "u_size");
+  gl.uniform1f(u_size, size);
+
+  const u_color = gl.getUniformLocation(webGLProgram,"u_color");
+  gl.uniform3f(u_color,1.0,0.0,0.0);
 
   gl.drawArrays(gl.POINTS, 0, 1);
 }
