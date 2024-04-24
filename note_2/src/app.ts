fc43539be65574954d5a4df6f51de34c467adc89
XU Cong <lavalselike@gmail.com>
2024-04-24 23:27:56 +0900

    try attribute

--- note_2/src/app.ts
+++ note_2/src/app.ts
@@ -6,9 +6,12 @@ let gl: WebGLRenderingContext = <WebGLRenderingContext> canvas.getContext("webgl
 
 //vertexShader, fragmentShader
 let vertexSource = `
+attribute vec2 a_position;
+attribute float a_size;
+
 void main(){
-  gl_Position = vec4(0.0,0.0,0.0,1.0);
-  gl_PointSize = 10.0;
+  gl_Position = vec4(a_position,0.0,1.0);
+  gl_PointSize = a_size;
 }
 `;
 
@@ -18,11 +21,26 @@ void main(){
 }
 `;
 
-const webGLProgram = initShaders(gl,vertexSource,fragmentSource);
-
-console.log(webGLProgram);
+const webGLProgram = <WebGLProgram>initShaders(gl,vertexSource,fragmentSource);
 
 gl.clearColor(0.0,0.0,0.0,1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-gl.drawArrays(gl.POINTS, 0, 1);
+let x = 0;
+let y = 0;
+let r = 0.5;
+let n = 1000;
+let size = 0;
+for(let i=0; i<n; i++){
+  r = i/n;
+  x = Math.sin(i) * r;
+  y = Math.cos(i) * r;
+  size = i / n * 8;
+  const a_position = gl.getAttribLocation(webGLProgram, "a_position");
+  gl.vertexAttrib2f(a_position, x, y);
+
+  const a_size = gl.getAttribLocation(webGLProgram, "a_size");
+  gl.vertexAttrib1f(a_size, size);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
+}
